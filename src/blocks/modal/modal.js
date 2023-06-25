177fd3d674667c1323de2af210db80bd847cc282
Sergey Filimonov <srfilimonoff@gmail.com>
2023-06-25 20:59:32 +0300

    add comment for modal.js

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -1,34 +1,32 @@
 (() => {
-
-const openButton = document.querySelector('.header__login');
-const closeButton = document.querySelector('.modal__close-button');
-const modal = document.querySelector('.modal');
-const togglePasswordButton = document.querySelector('.modal__toggle-password');
-const passwordInput = document.querySelector('#password');
-
-const toggleModal = () => {
-  modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
-};
-
-openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
-
-togglePasswordButton.addEventListener('click', () => {
-  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordInput.setAttribute('type', type);
-
-  // Добавьте код здесь, чтобы изменить изображение
+  const openButton = document.querySelector('.header__login');
+  const closeButton = document.querySelector('.modal__close-button');
+  const modal = document.querySelector('.modal');
+  const togglePasswordButton = document.querySelector(
+    '.modal__toggle-password'
+  );
+  const passwordInput = document.querySelector('#password');
   const imgElement = document.querySelector('.modal__toggle-img');
-  if (type === 'text') {
-    // Измените атрибут src элемента img, чтобы показать другое изображение
-    imgElement.src = 'images/eye-open.svg';
-  } else {
-    // Измените атрибут src элемента img обратно на исходное изображение
-    imgElement.src = 'images/eye-close.svg';
-  }
-});
 
+  // функция переключения модального окна
+  const toggleModal = () => {
+    modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+  };
+  // добовляем обработчик событий при клике
+  openButton.addEventListener('click', toggleModal);
+  closeButton.addEventListener('click', toggleModal);
+  // Добавляем обработчик событий к элементу
+  togglePasswordButton.addEventListener('click', () => {
+    const type =
+      passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
 
-
-
+    if (type === 'text') {
+      // Если тип равен 'text', меняем атрибут src элемента img на новое изображение
+      imgElement.src = 'images/eye-open.svg';
+    } else {
+      // В противном случае меняем атрибут src элемента img обратно на исходное изображение
+      imgElement.src = 'images/eye-close.svg';
+    }
+  });
 })();
