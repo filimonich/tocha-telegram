230b3015f06996990138110a1a475803a2410a13
Sergey Filimonov <srfilimonoff@gmail.com>
2023-06-26 17:28:00 +0300

    test2 fix build

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
-// import injectHTML from 'vite-plugin-html-inject';
-// import sassGlobImports from 'vite-plugin-sass-glob-import';
-
-// export default {
-//   root: './src',
-//   publicDir: './../public',
-//   build: {
-//     outDir: './../docs',
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       input: {
-//         main: '/index.html',
-//         other: '/pages/modal.html',
-//       },
-//     },
-//   },
-
-//   plugins: [injectHTML(), sassGlobImports()],
-// };
-
 import { defineConfig } from 'vite';
 import injectHtml from 'vite-plugin-html-inject';
 import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 export default defineConfig({
+  base: '',
   root: './src',
   publicDir: './../public',
   build: {
