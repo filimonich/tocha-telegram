f995735263743e285b54cb3ed3908ce610d08151
Sergey Filimonov <srfilimonoff@gmail.com>
2023-06-29 12:44:02 +0300

    feat: add modal function, hide when click outside the window modal

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -3,6 +3,7 @@
   const closeButton = document.querySelector('.modal__close-button');
   const modal = document.querySelector('.modal');
   const togglePasswordButton = document.querySelector('.modal__toggle-password');
+  const modalContent = document.querySelector('.modal__content');
   const passwordInput = document.querySelector('#password');
 
   // функция переключения модального окна
@@ -12,7 +13,22 @@
   // добовляем обработчик событий при клике
   openButton.addEventListener('click', toggleModal);
   closeButton.addEventListener('click', toggleModal);
-  // добавляем обработчик событий к элементу
+
+  // Добавляем обработчик событий click к элементу модального окна
+  modal.addEventListener('click', e => {
+    // Проверяем, было ли нажатие вне элемента modal__content
+    if (!modalContent.contains(e.target)) {
+      // Метод contains возвращает true, если узел является потомком указанного узла,
+      // иначе - false. В данном случае, если элемент,
+      // на который было нажатие (event.target),
+      // не является потомком элемента modalContent, то метод вернет false,
+      // и условие !modalContent.contains(event.target) будет истинным.
+      // Скрываем модальное окно, устанавливая свойство display в none
+      modal.style.display = 'none';
+    }
+  });
+
+  // добавляем обработчик для показа и скрытия пароля
   togglePasswordButton.addEventListener('click', () => {
     const type =
       passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
