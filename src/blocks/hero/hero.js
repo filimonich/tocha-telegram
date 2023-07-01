674936f0d690ee75f55477a182e007447a37ebe2
Sergey Filimonov <srfilimonoff@gmail.com>
2023-07-01 22:48:53 +0300

    feat: prescribe for the test hero.js

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,4 +1,14 @@
+import { ContentManager } from '/js/animation.js';
 (() => {
+
+const options = {
+  contentSelector: '.content',
+  heroHeadlineSelector: '.hero__headline',
+  text: 'Telegram чат\nна вашем сайте',
+  typeDelay: 200
+};
+
+const contentManager = new ContentManager(options);
 
 
 })();
