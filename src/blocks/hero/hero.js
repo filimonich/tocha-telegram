d59617bd8a621e301c91731f2fe1ce44b5ef10e5
Sergey Filimonov <srfilimonoff@gmail.com>
2023-07-03 13:53:27 +0300

    refactor: add comments and refactoring

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,60 +1,18 @@
-// import { textPrinting, observeContents } from '/js/animations-appearance.js';
+import { textPrinting, observeContainers, containers } from '/js/animations-appearance.js';
 
+(() => {
+  const textContent = 'Telegram чат\nна вашем сайте';
+  const content = document.querySelector('.hero__headline');
+  let i = 0;
+  const className = 'telegram-chat';
+  const selectors = '.hero__title, .header, .hero__picture';
 
-// export 
-function textPrinting(text, contentBr, i, className) {
-  if (i < text.length) {
-    if (text[i] === '\n') {
-      contentBr.appendChild(document.createElement('br'));
-    } else {
-      const spanItem = document.createElement('span');
-      if (i >= 0 && i < 12) {
-        spanItem.classList.add(className);
-      }
-      spanItem.innerText = text[i];
-      contentBr.appendChild(spanItem);
-    }
-    i++;
-    setTimeout(() => textPrinting(text, contentBr, i, className), 100);
-  }
-  return i;
-}
-
-// создать экземпляр класса IntersectionObserver для отслеживания
-// пересечения элементов с viewport
-const observer = new IntersectionObserver(handleIntersect);
-
-
-// export 
-function observeContents(contents, selectors) {
-  contents.forEach(content => observer.observe(content));
-}
-
-function handleIntersect(entries, selectors) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-      entry.target
-        .querySelectorAll(selectors)
-        .forEach(el => el.classList.add('visible'));
-    }
-  });
-}
-
-
-
-
-const contents = document.querySelectorAll('.content');
-const text = 'Telegram чат\nна вашем сайте';
-const content = document.querySelector('.hero__headline');
-let i = 0;
-const className = 'telegram-chat';
-const selectors = '.hero__title, .header, .hero__picture';
-
-observeContents(contents, selectors);
-setTimeout(() => {
-  i = textPrinting(text, content, i, className);
-}, 3000);
+  observeContainers(containers, selectors);
+  // Вызываем функцию observeContents для начала наблюдения за элементами containers
+  setTimeout(() => {
+    i = textPrinting(textContent, content, i, className);
+  }, 3000);
+})();
 
 // добавляет классы "visible" поочередно к элементам для запуска анимаций
 
--- src/js/animations-appearance.js
+++ src/js/animations-appearance.js
@@ -1,36 +1,51 @@
+export const containers = document.querySelectorAll('.container');
 
-// export function type(text, content, i, className) {
-//   if (i < text.length) {
-//     if (text[i] === '\n') {
-//       content.appendChild(document.createElement('br'));
-//     } else {
-//       const span = document.createElement('span');
-//       if (i >= 0 && i < 12) {
-//         span.classList.add(className);
-//       }
-//       span.innerText = text[i];
-//       content.appendChild(span);
-//     }
-//     i++;
-//     setTimeout(() => type(text, content, i, className), 100);
-//   }
-//   return i;
-// }
+// экспорт функции, которая аечатает текст
+export function textPrinting(textContent, brCreate, i, className) {
+  if (i < textContent.length) {
+    // проверка, если i меньше длины текста
+    if (textContent[i] === '\n') {
+      // если есть \n заменить на элемент <br>
+      brCreate.appendChild(document.createElement('br'));
+    } else {
+      // иначе создать элемент <span>
+      const spanCreate = document.createElement('span');
+      if (i >= 0 && i < 12) {
+        // если i в диапазоне от 0 до 12, то добавить className к span
+        spanCreate.classList.add(className);
+      }
+      spanCreate.innerText = textContent[i];
+      // установить текстовое содержимое span равным текущему симвулу текста
+      brCreate.appendChild(spanCreate);
+      // добавить span в spanCreate
+    }
+    i++; // увеличить индекс i на 1
+    setTimeout(() => textPrinting(textContent, brCreate, i, className), 100);
+    // вызов функции с задержкой
+  }
+  return i; // возврат текущего значения i
+}
 
-// const observer = new IntersectionObserver(handleIntersect);
+// создать экземпляр класса IntersectionObserver для отслеживания
+// пересечения элементов с viewport
+const observer = new IntersectionObserver(handleIntersect);
 
-// export function observeContents(contents, selectors) {
-//   contents.forEach(content => observer.observe(content));
-// }
+export function observeContainers(containers) {
+  // наблюдение за элементами
+  containers.forEach(content => observer.observe(content));
+}
 
-// function handleIntersect(entries, selectors) {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add('visible');
-//       entry.target
-//         .querySelectorAll(selectors)
-//         .forEach(el => el.classList.add('visible'));
-//     }
-//   });
-// }
-
+function handleIntersect(entries, selectors) {
+  // handleIntersect вызывается при пересечении элементов с viewport
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // если entry пересекается с viewport
+      entry.target.classList.add('visible');
+      // Добавить класс 'visible' к элементу entry.target
+      entry.target
+        .querySelectorAll(selectors)
+        .forEach(el => el.classList.add('visible'));
+      // для каждого элемента, selectors внутри entry.target
+    }
+  });
+}
