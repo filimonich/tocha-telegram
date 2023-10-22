516c157fb51603a77b79cbf6a452ea7df55e4a67
Sergey Filimonov <srfilimonoff@gmail.com>
2023-10-22 13:15:15 +0300

    feat: add logic of the drop-down menu

--- src/blocks/hamburger/hamburger.js
+++ src/blocks/hamburger/hamburger.js
@@ -1,4 +1,42 @@
 (() => {
+  (() => {
+    // открытие и закрытие меню и меню-профиля
+    const hamburgerButton = document.querySelector('.hamburger-js');
+    const menuElement = document.querySelector('.menu');
+    const dropMenuProfile = document.querySelector('.header__menu-drop');
+    const menuProfile = document.querySelector('.header__menu-profile');
 
+    function toggleMainMenu() {
+      hamburgerButton.classList.toggle('hamburger--active');
+      menuElement.classList.toggle('menu--active');
+      menuElement.classList.add('menu--close');
+      setTimeout(() => {
+        menuElement.classList.remove('menu--close');
+      }, 2000);
+    }
 
+    function closeMainMenu() {
+      hamburgerButton.classList.remove('hamburger--active');
+      menuElement.classList.remove('menu--active');
+      setTimeout(() => {
+        menuElement.classList.remove('menu--close');
+      }, 200);
+    }
+
+    function handleMainMenuItemClick(event) {
+      if (event.target.classList.contains('menu__link')) {
+        toggleMainMenu();
+      }
+    }
+
+    function handleDocumentClick(event) {
+      if (event.target !== hamburgerButton) {
+        closeMainMenu();
+      }
+    }
+
+    menuElement.addEventListener('click', handleMainMenuItemClick);
+    hamburgerButton.addEventListener('click', toggleMainMenu);
+    document.addEventListener('click', handleDocumentClick);
+  })();
 })();
